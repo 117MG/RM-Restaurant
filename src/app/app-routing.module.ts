7362e6cba418a1b2aff8f070e1529939da382361
117MG <117.mayankgupta@gmail.com>
2021-08-03 21:49:39 +0530

    home created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {AboutUsComponent} from './components/about-us/about-us.component';
+import {ContactUsComponent} from './components/contact-us/contact-us.component';
+import {ExtraComponent} from './components/extra/extra.component';
+import {HomeComponent} from './components/home/home.component';
+import {LocationComponent} from './components/location/location.component';
+import {MenuComponent} from './components/menu/menu.component';
+import {ReservationComponent} from './components/reservation/reservation.component';
+
+const routes: Routes = [
+
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'aboutUs',
+    component:AboutUsComponent
+  },
+  {
+    path:'menu',
+    component:MenuComponent
+  },
+  {
+    path:'reservation',
+    component:ReservationComponent
+  },
+  {
+    path:'contactUs',
+    component:ContactUsComponent
+  },
+  {
+    path:'location',
+    component:LocationComponent
+  },
+  {
+    path:'extra',
+    component:ExtraComponent
+  }
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { LocationComponent } from './components/location/location.component';
+import { ExtraComponent } from './components/extra/extra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    MenuComponent,
+    ReservationComponent,
+    ContactUsComponent,
+    LocationComponent,
+    ExtraComponent
   ],
   imports: [
     BrowserModule,
